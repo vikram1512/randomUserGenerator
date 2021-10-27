7d229eba763e9d41af2eb783bb2221b8ed7684c1
vikram <vikramvarmakunaparaju@gmail.com>
2021-10-26 21:15:14 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: "Vikram",
+      lastName: "Kunaparaju",
+      emailAddress: "kvvvarma547@gmail.com",
+      Gender: "male",
+      picture: "https://randomuser.me/api/portraits/men/10.jpg",
+    };
+  },
+
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+
+      const { results } = await res.json();
+
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.emailAddress = results[0].email;
+      this.Gender = results[0].gender;
+      this.picture = results[0].picture.large;
+    },
+  },
+});
+
+app.mount("#app");
